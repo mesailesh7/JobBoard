187b34e57684edc79b93697d39a688900ef657d7
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-04-08 09:28:59 -0600

    useEffect implemented

--- pages/apply/[id]/index.jsx
+++ pages/apply/[id]/index.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import { getJob } from "@/utils/api/jobs";
 
 const applyJob = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const jobData = await getJob(id);
+        setJob(jobData);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+
+      fetchJob();
+    };
+  }, [router.isReady]);
+
   return <div>applyJob {id}</div>;
 };
 
