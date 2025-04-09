daba44c0bc096e95ffc0a02de1678a9ae9c33642
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-04-08 22:49:18 -0600

    Bug [form submission giving error]

--- components/apply/ApplyJobForm.js
+++ components/apply/ApplyJobForm.js
@@ -1,29 +1,53 @@
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import { useState, useEffect } from "react";
+import { postApplication } from "@/utils/api/jobs";
 
-export default function ApplyJobForm({job, submitCallback}) {
-  return <form style={{width: `90%`, }}>
-    <Stack direction="column" spacing={2}>
-      <TextField
-        id="full-name"
-        label="Full Name"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        id="email"
-        label="email"
-        variant="outlined"
-        fullWidth
-      />
-      <Button
-        variant="contained"
-        color="success"
-        type="submit"
-      >
-        Submit Application
-      </Button>
-    </Stack>
-  </form>
+export default function ApplyJobForm({ job, submitCallback }) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const application = {
+      jobId: job.id,
+      fullName: fullName,
+      email: email,
+    };
+
+    const result = await postApplication(application);
+
+    if (result) {
+      submitCallback();
+    }
+  };
+
+  return (
+    <form style={{ width: `90%` }}>
+      <Stack direction="column" spacing={2}>
+        <TextField
+          id="full-name"
+          label="Full Name"
+          variant="outlined"
+          fullWidth
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+        <TextField
+          id="email"
+          label="email"
+          variant="outlined"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+        />
+        <Button variant="contained" color="success" type="submit">
+          Submit Application
+        </Button>
+      </Stack>
+    </form>
+  );
 }
